3593d3a7ea3af17e16a2e5cdc22ffc2dad599996
shayne2000 <st3334roongaroon.ac.th@gmail.com>
2026-09-26 10:21:12 +0700

    Seperate files by class

---
+++ src/main/java/thisFolder/Booking.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package thisFolder;
+
+/**
+ *
+ * @author Shayne
+ */
+
+
+
+public class Booking {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("test");
+    }
+}
