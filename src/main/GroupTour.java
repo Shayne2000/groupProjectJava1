70049152b58cecb86a080705232fdd53ee28c968
Ch1non <sornchai.soontranon054@gmail.com>
2026-09-26 17:28:33 +0700

    gt

---
+++ src/main/GroupTour.java
@@ -0,0 +1,26 @@
+package Project1;
+
+public class GroupTour extends Tour {
+    private final double rate15To20;
+    private final double rate21To30;
+    private final double rate31Plus;
+    private final double singleSupplement;
+
+    public GroupTour(String code, double rate15To20, double rate21To30,
+                     double rate31Plus, double singleSupplement) {
+        super(code);
+        this.rate15To20 = rate15To20;
+        this.rate21To30 = rate21To30;
+        this.rate31Plus = rate31Plus;
+        this.singleSupplement = singleSupplement;
+    }
+
+    @Override
+    public double calculatePayment(int persons, int singleRequests) {
+        double rate;
+        if (persons <= 20) rate = rate15To20;
+        else if (persons <= 30) rate = rate21To30;
+        else rate = rate31Plus;
+        return persons * rate + singleRequests * singleSupplement;
+    }
+}
